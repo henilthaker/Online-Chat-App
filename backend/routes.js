c72c6a11cad2da432672df136aedbe15a8fdd7e9
Darshan <darshanbpatel1904@gmail.com>
2023-06-21 00:43:03 +0530

    Added routes for message and user

--- backend/routes.js
+++ backend/routes.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const { addChat, getChat } = require('./controllers/chat_controller.js');
 const { uploadFile, getFile } = require('./controllers/file_controller.js');
+const { newMessage, getMessage } = require('./controllers/message_controller.js');
+const { addUser, getUser } = require('./controllers/user_controller.js');
 const upload = require('./utils/upload.js');
 
 // setup router 
@@ -9,7 +11,17 @@ const router = express.Router();
 
 // chat routes
 router.post('/chats/add', addChat);
-router.post('/chats/get', getChat)
+router.post('/chats/get', getChat);
+
+
+// message routes
+router.post('/message/new', newMessage);
+router.get('/message/get/:id', getMessage);
+
+
+// user routes
+router.post('/user/add', addUser);
+router.get('/user/get', getUser);
 
 
 // file routes
