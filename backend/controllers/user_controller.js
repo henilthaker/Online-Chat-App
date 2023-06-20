51cab925bc0e35bf821d63ffb23fbbd08e0b466a
Darshan <darshanbpatel1904@gmail.com>
2023-06-20 23:46:23 +0530

    Added message and user controllers and message model

---
+++ backend/controllers/user_controller.js
@@ -0,0 +1,30 @@
+//imports
+import User from "../modal/User.js";
+
+//Add new User function
+export const addUser = async (request, response) => {
+    try {
+        let exist = await User.findOne({ sub: request.body.sub });
+
+        if(exist) {
+            response.status(200).json('user already exists');
+            return;
+        }
+
+        const newUser = new User(request.body);
+        await newUser.save();
+        response.status(200).json(newUser);
+    } catch (error) {
+        response.status(500).json(error);
+    }
+}
+
+//Get all users data
+export const getUser = async (request, response) => {
+    try {
+        const user = await User.find({});
+        response.status(200).json(user);
+    } catch (error) {
+        response.status(500).json(error);
+    }
+}
