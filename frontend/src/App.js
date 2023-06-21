d0e24617a2469ffb23ec8677a1c062be9e92fab1
Darshan <darshanbpatel1904@gmail.com>
2023-06-21 23:17:01 +0530

    Added google authentication usign googleAuth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import SignUp from "./components/SignUp";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+//APP
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider className="App" clientId={process.env.REACT_APP_client_ID}>
+      <SignUp />
+      <ToastContainer />
+    </GoogleOAuthProvider>
   );
 }
 
