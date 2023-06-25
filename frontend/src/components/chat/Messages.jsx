12403e1deb8e99dbdd89b360c76c7e66cc9c5b4f
Henil Thaker <henilthaker2010@gmail.com>
2023-06-25 15:48:55 +0530

    added timestamps and date

--- frontend/src/components/chat/Messages.jsx
+++ frontend/src/components/chat/Messages.jsx
@@ -1,6 +1,6 @@
 import { Box, styled } from '@mui/material';
 import AccountContext from '../../context/AccountDetails';
-import { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import axios from '../../Axios.js';
 import '../../styles/message.css';
 
@@ -17,10 +17,34 @@ const Component = styled(Box)`
     overflow-y: scroll;
 `;
 
+function getDay(date) {
+    // Get the day, month, and year from the date object.
+    let day = new Date(date).getDate();
+    let month = new Date(date).getMonth() + 1;
+    let year = new Date(date).getFullYear();
+
+    // Pad the day and month with zeros if they are less than 10.
+    if (day < 10) {
+        day = "0" + day;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+
+    // Return the date in the format dd-mm-yyyy.
+    return `${day}-${month}-${year}`;
+}
+
+const formatDate = (date) => {
+    const hours = new Date(date).getHours();
+    const minutes = new Date(date).getMinutes();
+    return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+}
 
 const Messages = () => {
     const { account, chat } = useContext(AccountContext);
     const [messages, setMessages] = useState([]);
+    let cur_date = '';
     useEffect(() => {
         const getMessages = async () => {
             try {
@@ -33,17 +57,33 @@ const Messages = () => {
 
         getMessages();
     }, [chat]);
-    console.log(messages.length);
+
     return (
         <Wrapper>
             <Component>
-                {messages && messages.map(message => (
-                    <div className = "messageContainer" key={message._id}>
-                        <Box className={`messageBox ${message.senderId === account.sub && 'sentMessage'}`}>
-                            {message.text}
-                        </Box>
-                    </div>
-                ))}
+                {messages && messages.map(message => {
+                    const message_day = getDay(message.updatedAt);
+                    let flag = true;
+                    if (message_day !== cur_date) {
+                        cur_date = message_day;
+                        flag = false;
+                    }
+                    return (
+                        <React.Fragment key={message._id}>
+                            {
+                                !flag && <Box className="date">
+                                    {message_day}
+                                </Box>
+                            }
+                            <div className="messageContainer">
+                                <Box className={`messageBox ${message.senderId === account.sub && 'sentMessage'}`}>
+                                    {message.text}
+                                    <span className="time">{formatDate(message.updatedAt)}</span>
+                                </Box>
+                            </div>
+                        </ React.Fragment>
+                    )
+                })}
             </Component>
             <Footer />
         </Wrapper>
