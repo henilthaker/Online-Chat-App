894d2b5499b3498724824125321fe7274ab99ab7
Henil Thaker <henilthaker2010@gmail.com>
2023-07-29 17:09:19 +0530

    changed landing page and empty chat page appearence

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,36 @@
-import SignUp from "./components/SignUp";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useContext, useEffect} from "react";
+import { useContext, useEffect } from "react";
 import AccountContext from "./context/accountContext/AccountDetails";
 import Home from "./pages/Home.jsx";
+import LandingPage from "./pages/LandingPage";
 import Pusher from 'pusher-js';
 //APP
 function App() {
   const { account, setUsers, users } = useContext(AccountContext);
 
   useEffect(() => {
-      const pusher = new Pusher('8de87b75a39cda78cd32', {
-        cluster: 'ap2'
-      });
+    const pusher = new Pusher('8de87b75a39cda78cd32', {
+      cluster: 'ap2'
+    });
 
-      const channel = pusher.subscribe('users');
-      channel.bind('loggedIn', (newUser) => {
-        setUsers([...users, newUser])
-      });
+    const channel = pusher.subscribe('users');
+    channel.bind('loggedIn', (newUser) => {
+      setUsers([...users, newUser])
+    });
 
-      return () => {
-        channel.unbind_all();
-        channel.unsubscribe();
-      }
-      
-  },[users, setUsers]);
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+
+  }, [users, setUsers]);
 
   return (
     <div className="app">
       {
         account ? <Home />
           :
-          <GoogleOAuthProvider className="App" clientId={process.env.REACT_APP_client_ID}>
-              <SignUp />
-              <ToastContainer />
-          </GoogleOAuthProvider>
+          <LandingPage />
       }
     </div>
   );
